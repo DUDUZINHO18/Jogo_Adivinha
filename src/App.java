c188dedfded21312a9a5cdea7e583e1f89f72f39
DUDUZINHO_MM <jeaneduardo0505@gmail.com>
2025-04-19 02:12:08 -0300

    main

--- src/App.java
+++ src/App.java
@@ -1,32 +1,57 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        double numeroAcertar = 3.14;
-        int tentativas = 0;
-        int maxTentativas = 5;
-        
+        //Scanner para ler o nome do usuário
         Scanner entradaDados = new Scanner(System.in);
 
-        System.out.println("<===Jogo do Adivinha===> \nO Jogador precisa escolher um número de 0 10. Números quebrados não são excessão! \n ATENÇÃO PARA AS REGRAS DO JOGO! \n O Jogador terá 3 tentativas. Se acertar, sera Parabenizado. Se o jogador errar, você terá sua sessão encerrada. \nBoa Sorte! :)");
+        // Instancia o objeto Random para gerar números aleatórios
+        Random random = new Random();
 
-        double numeroDigitado = 0;
+        // Solicita o nome do jogador para personalização
+        System.out.println("=================================================");
+        System.out.println(" B O R D E R L A N D  -  Ú L T I M O  -  J O G O ");
+        System.out.println("=================================================");
+        System.out.print("Como devemos chama-lo? ");
+        
+        String nome = entradaDados.nextLine().trim(); // Lê o nome do jogador e remove espaços em branco no início e no fim
+        
+        // Apresentação inicial do jogo com o nome do jogador
+        System.out.println("O BORDERLAND, um dos melhores jogos de emoções, agradece pela preferencia.");   
+        System.out.println("Como sinal de agradecimento, daremos inicio a sua jornada com um jogo de adivinhações."); 
+        System.out.println("Você possue 3 passes de sobrevivencia.");
 
-        while (numeroDigitado != numeroAcertar) {
-
-            numeroDigitado = entradaDados.nextDouble();
+        int vida = 3; // Inicializa a vida do jogador com 3 passes de sobrevivência
+        int fase = 1;
+        boolean vivo = true; // Inicializa a variável de controle de vida do jogador
+        
+        // Loop principal do jogo que executa enquanto o jogador estiver vivo e houver fases
+        while (vivo && fase <= 3) {
+            System.out.println("=================================================");
+            System.out.println("DIFICULDADE: 'E U R U S' ");
             
-            if (numeroDigitado != numeroAcertar) {
-                System.out.println("Número errado, tente novamente: ");
+            // -------------------------
+            // FASE 1 – NÚMERO FANTASMA
+            // -------------------------
+
+            if (fase == 1) {
+                
+                System.out.println("Atenção!\nExplicaremos as regras do Jogo.");
+                System.out.println("Aqui, sobrevivência é mérito, erro é sentença.");
+                System.out.println("Você foi escolhido(a) para participar do jogo. Não há mais volta, apenas uma escolha: Jogar ou ser Eliminado.");
+                System.out.println("Se você não jogar o jogo, ele jogara você.");
+                System.out.println("Sobreviva às 3 fases desafiadoras do jogo, cada uma com regras e armadilhas únicas.\n" + "Você começa com 3 passes de sobrevivência (vidas). A cada erro, uma vida é perdida.");
+                
+                System.out.println("============================");
+                System.out.println("N Ú M E R O  F A N T A S M A");
+                System.out.println("============================");
+
+                System.out.println("Um número entre 1 e 10 foi escolhido pela máquina, você tem 3 tentativas para adivinhar o número.\n" + "Erre todas, e perderá 1 vida. Se acertar, passara para a p´roxima fase. Boa Sorte! ;)");
             }
-            else {
-                System.out.println("Parabéns, você acertou!");
-            }
+            
+        
         }
-        
-
-
-        
     }
 }
